727b2694dc938fed1478f06c82a25069a01c789d
Daisuke Ogata <daisule.ogata.work@gmail.com>
2019-04-30 17:37:06 +0900

    顧客追加画面を実装

--- src/main/java/hello/hello/controller/CustomerController.java
+++ src/main/java/hello/hello/controller/CustomerController.java
@@ -6,10 +6,7 @@ import hello.hello.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/customer")
@@ -33,12 +30,17 @@ public class CustomerController {
         return "customer/list";
     }
 
-    @GetMapping(path="/add")
-    public @ResponseBody String addCustomer(@RequestParam String name, @RequestParam int age) {
+    @GetMapping(path="/create")
+    public String createCustomer() {
+        return "customer/create";
+    }
+
+    @PostMapping(path="/add")
+    public String addCustomer(@RequestParam String name, @RequestParam int age) {
         Customer customer = new Customer();
         customer.setName(name);
         customer.setAge(age);
         customerRepository.save(customer);
-        return "Saved";
+        return "customer/complete";
     }
 }
