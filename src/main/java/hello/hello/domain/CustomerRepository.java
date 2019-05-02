27a322c08b2f33575316fde8719ba402efc784aa
Daisuke Ogata <daisule.ogata.work@gmail.com>
2019-05-02 17:44:47 +0900

    名前のLIKE検索を実装

--- src/main/java/hello/hello/domain/CustomerRepository.java
+++ src/main/java/hello/hello/domain/CustomerRepository.java
@@ -1,9 +1,11 @@
 package hello.hello.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    public List<Customer> findByName(String name);
+    @Query("select c from Customer c where c.name like %:name% order by c.id asc")
+    public List<Customer> findByNameLike(String name);
 }
